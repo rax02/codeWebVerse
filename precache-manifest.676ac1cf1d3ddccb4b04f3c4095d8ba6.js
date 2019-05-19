a1517e91b3e9320c4732b618bc178c83b0a05e4a
Ravi <0001rkm@gmail.com>
2019-05-19 22:23:53 +0530

    Updates

---
+++ precache-manifest.676ac1cf1d3ddccb4b04f3c4095d8ba6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7fc28122125bb9e6b867fe9246d976f",
+    "url": "/rax02/codeWebVerse/static/media/softwareEngineer.a7fc2812.png"
+  },
+  {
+    "revision": "13b3352a30e10d0c511cf84b155c955d",
+    "url": "/rax02/codeWebVerse/static/media/reactLogo.13b3352a.png"
+  },
+  {
+    "revision": "664a8c00ef526bf33edf55808601d461",
+    "url": "/rax02/codeWebVerse/static/media/htmlLogo.664a8c00.png"
+  },
+  {
+    "revision": "e517d51d4e38313cc9b8",
+    "url": "/rax02/codeWebVerse/static/js/runtime~main.e517d51d.js"
+  },
+  {
+    "revision": "e1614eddda4545b78563",
+    "url": "/rax02/codeWebVerse/static/js/main.e1614edd.chunk.js"
+  },
+  {
+    "revision": "4b360956a2cb1ab44439",
+    "url": "/rax02/codeWebVerse/static/js/2.4b360956.chunk.js"
+  },
+  {
+    "revision": "e1614eddda4545b78563",
+    "url": "/rax02/codeWebVerse/static/css/main.37a75ec2.chunk.css"
+  },
+  {
+    "revision": "f300846f7272806870134fe3ca6a4400",
+    "url": "/rax02/codeWebVerse/index.html"
+  }
+];
